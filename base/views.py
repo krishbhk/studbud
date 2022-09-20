3649c06bae27b35478a809f5dfbb2fe837209a4b
rishkola <rishabh.kumar2@olacabs.com>
2022-09-20 15:09:11 +0530

    Skeleton of project

--- base/views.py
+++ base/views.py
@@ -1,11 +1,17 @@
+from multiprocessing import context
 from django.shortcuts import render
 
-# Create your views here.
-
 from django.http import HttpResponse
+
+rooms = [
+            {'id': 1, 'name':'Django Tuts'},
+            {'id': 2, 'name':'NodeJS tuts'},
+            {'id': 3, 'name':'Springboot Tuts'}
+        ]
 
 def home(request):
     return render(request, 'home.html')
 
 def room(request):
-    return render(request, 'room.html')
+    context = {'rooms': rooms}
+    return render(request, 'room.html', context)
